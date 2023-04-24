262372bb2890ab39a8d218acbbcdf9862b155832
Arrue Marianne <5608@holbertonstudents.com>
2023-04-24 04:53:15 -0700

    add API status and visualisation of this status : add/remove class available

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  const amenities = {};
+  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  $.get(url, function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
+
+  $('input[type="checkbox"]').change(function () {
+    if ($(this).is(':checked')) {
+      amenities[$(this).data('id')] = $(this).data('name');
+    } else {
+      delete amenities[$(this).data('id')];
+    }
+    const amenity_names = Object.values(amenities).join(', ');
+    $('.amenities h4').text(amenity_names);
+  });
+});
