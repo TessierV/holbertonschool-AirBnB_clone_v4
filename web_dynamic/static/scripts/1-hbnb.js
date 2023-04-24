13fd15cddb580da5c435f7df1419de72fd395a7b
TessierV <5628@holbertonstudents.com>
2023-04-24 04:52:53 -0700

    save before pull

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,17 +1,12 @@
-$(document).ready(function(){
+$('document').ready(function () {
     const amenities = {};
-    const amenities_check = 'input[type="checkbox"]';
-    $(amenities_check).change(function() {
-        if ($(this).is(':checked')) {
-            amenities[$(this).data('id')] = $(this).data('name');
-            $(this).css({
-                'position': 'absolute',
-                'left': ($(this).offset().left - 10) + 'px'
-            });
-        } else {
-            delete amenities[$(this).data('id')];
-        }
-        let amenity_names = Object.values(amenities).join(', ');
-        $('.amenities h4').text(amenity_names);
+    $('input[type="checkbox"]').change(function () {
+      if ($(this).is(':checked')) {
+        amenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenities[$(this).data('id')];
+      }
+      const amenity_names = Object.values(amenities).join(', ');
+      $('.amenities h4').text(amenity_names);
     });
 });
