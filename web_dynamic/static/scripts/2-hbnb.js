c070875486b4428946e97c4b12937c4fab25a979
Arrue Marianne <5608@holbertonstudents.com>
2023-04-25 05:36:52 -0700

    solve conflict

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,11 +1,11 @@
 $(document).ready(function () {
   const amenities = {};
-  const url = 'http://0.0.0.0:5001/api/v1/status/';
+  const url = 'http://127.0.0.1:5001/api/v1/status/';
   $.get(url, function (data) {
     if (data.status === 'OK') {
-      $('#api_status').addClass('available');
+      $('div#api_status').addClass('available');
     } else {
-      $('#api_status').removeClass('available');
+      $('div#api_status').removeClass('available');
     }
   });
 
